9903824172849a6d5497ca4000a2178e6cb130e1
Alex Shelley <alexthepeppershelley@gmail.com>
2020-09-26 17:40:02 -0400

    initial commit

---
+++ Assets/CylinderPhysics.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CylinderPhysics
+{
+    private const float ENDLESS_DISTANCE = Mathf.Infinity;
+    
+    public static bool CylinderCast(
+        Vector3 origin,
+        float radius,
+        float height,
+        Vector3 direction,
+        float maxDistance = ENDLESS_DISTANCE
+    )
+    {
+        return CylinderCastAll(origin, radius, height, direction, maxDistance).Length > 0;
+    }
+
+    public static RaycastHit[] CylinderCastAll(
+        Vector3 origin,
+        float radius,
+        float height,
+        Vector3 direction,
+        float maxDistance = ENDLESS_DISTANCE
+    )
+    {
+        Debug.DrawLine(origin, origin + (direction * maxDistance));
+        Debug.DrawLine(origin, origin + Vector3.left * radius);
+        Debug.DrawLine(origin, origin + Vector3.right * radius);
+        Debug.DrawLine(origin, origin + Vector3.forward * radius);
+        Debug.DrawLine(origin, origin + Vector3.back * radius);
+
+        // Unity doesn't have support for cylinder collisions.
+        // NO MATTER.
+        // We can just do two casts: a box cast and a capsule cast.
+        // Anything that passes the box cast is within the cylinder's "height".
+        // Anything that passes the capsule cast is within the cylinder's "radius".
+        // Anything that passes BOTH casts must be in our cylinder.
+        // This essentially "trims off the corners" of the box cast.
+        // Note that this only works for convex shapes.  
+        // Just don't use concave platforms.
+        Quaternion orientation = Quaternion.identity;   // TODO: Derive this from direction
+        var halfExtents = new Vector3
+        (
+            radius,
+            height / 2,
+            radius
+        );
+        var boxPos = origin - new Vector3(0, height / 2, 0);
+        var boxHits = Physics.BoxCastAll(boxPos, halfExtents, direction, orientation, maxDistance);
+
+        // If any of the colliders from the boxcast ALSO pass a really tall
+        // capsule cast, then it's in the cylinder
+        var hits = new List<RaycastHit>();
+        foreach (var h in boxHits)
+        {
+            var capsulePointOffset = direction.normalized * (h.distance + radius);
+            bool inCapsule = Physics.CheckCapsule
+            (
+                origin + capsulePointOffset,
+                origin - capsulePointOffset,
+                radius
+            );
+            if (inCapsule)
+                hits.Add(h);
+        }
+        return hits.ToArray();
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private readonly float _groundDetectorThickness = 0.1f;
+    private readonly float _groundDetectorRadius = 0.5f;
+    private readonly float _gravity = 40;
+    private readonly float _hSpeedMax = 5;
+    private readonly float _hAccelMax = 5;
+    private readonly float _rotSpeedDeg = 360 * 2;
+    private readonly float _friction = 10;
+
+    private readonly float _coyoteTime = 0.1f;      // Allows you to press the jump button a little "late" and still jump
+    private readonly float _earlyJumpTime = 0.1f;   // Allows you to press the jump button a little "early" and still jump
+
+    private float _hAngle = 0;
+    private float _hSpeed = 0;
+    private float _vSpeed = 0;
+
+    private float _lastGroundedTime = 0;
+    private float _lastJumpButtonPressTime = 0;
+
+    private Transform _lastGroundTouched;
+    private Vector3 _lastPositionRelativeToGround;
+    private Vector3 _groundVelocity;
+
+    private CharacterController _controller;
+
+    public void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            _lastJumpButtonPressTime = Time.time;
+    }
+
+    public void FixedUpdate()
+    {
+        UpdateGroundVelocity();
+        _lastGroundTouched = GetGround();
+
+        ApplyGravityAndJumping();
+        ApplyHorizontalMovement();
+
+        // Compute the velocity vector and move
+        var velocity = new Vector3
+        (
+            _hSpeed * Mathf.Cos(_hAngle),
+            _vSpeed,
+            _hSpeed * Mathf.Sin(_hAngle)
+        );
+        velocity += _groundVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+
+        // Remember moving-platform stuff for next frame
+        if (_lastGroundTouched != null)
+            _lastPositionRelativeToGround = _lastGroundTouched.InverseTransformPoint(transform.position);
+    }
+
+    public void FixedUpdate_old()
+    {
+        Transform currentGround = GetGround();
+        bool isGrounded = currentGround != null;
+
+        // Move with the platform we're standing on
+        if (isGrounded)
+        {
+            if (_lastGroundTouched != null)
+            {
+                // Figure out where our "foot prints" have moved to
+                var currentFootprintsPos = currentGround.TransformPoint(_lastPositionRelativeToGround);
+                var lastFootprintsPos = transform.position;
+
+                // Figure out much much the footprints moved, and move by that much
+                var deltaFootprints = currentFootprintsPos - lastFootprintsPos;
+                _controller.Move(deltaFootprints);
+            }
+        }
+
+        ApplyGravityAndJumping();
+        ApplyHorizontalMovement();
+
+        // Compute the velocity vector and move
+        var velocity = new Vector3
+        (
+            _hSpeed * Mathf.Cos(_hAngle),
+            _vSpeed,
+            _hSpeed * Mathf.Sin(_hAngle)
+        );
+        _controller.Move(velocity * Time.deltaTime);
+
+        // Remember the moving platform values for next frame
+        _lastGroundTouched = currentGround;
+        if (isGrounded)
+            _lastPositionRelativeToGround = _lastGroundTouched.InverseTransformPoint(transform.position);
+    }
+
+    private void UpdateGroundVelocity()
+    {
+        if (_lastGroundTouched == null)
+        {
+            _groundVelocity = Vector3.zero;
+            return;
+        }
+
+        // Figure out where our "foot prints" have moved to
+        var currentFootprintsPos = _lastGroundTouched.TransformPoint(_lastPositionRelativeToGround);
+        var lastFootprintsPos = transform.position;
+
+        // Figure out how much the footprints moved, and move by that much
+        var deltaFootprints = currentFootprintsPos - lastFootprintsPos;
+        _groundVelocity = deltaFootprints / Time.deltaTime;
+    }
+
+    private void ApplyGravityAndJumping()
+    {
+        // Apply gravity
+        _vSpeed -= _gravity * Time.deltaTime;
+
+        // Stop falling if we hit the ground.
+        if (IsGrounded())
+        {
+            _lastGroundedTime = Time.time;
+            _vSpeed = 0;
+        }
+
+        // Jump when the button is pressed and we're on the ground.
+        // Well, OK, that's a little too strict.  
+        // We should let the player press the jump button a little bit before hitting the ground.
+        // And we should also let them do it a little bit after leaving the ground.
+        bool jumpPressedRecently = (Time.time - _earlyJumpTime < _lastJumpButtonPressTime);
+        bool wasGroundedRecently = (Time.time - _coyoteTime < _lastGroundedTime);
+        if (wasGroundedRecently && jumpPressedRecently)
+            _vSpeed = 20;
+    }
+
+    private void ApplyHorizontalMovement()
+    {
+        var inputVector = GetWalkInput();
+        
+        if (IsGrounded())
+        {
+            // Speed up/slow down with the left stick
+            float hSpeedIntended = inputVector.magnitude * _hSpeedMax;
+            float accel = _hSpeed < hSpeedIntended
+                ? _hAccelMax
+                : _friction;
+
+            _hSpeed = Mathf.MoveTowards(_hSpeed, hSpeedIntended, accel * Time.deltaTime);
+
+
+            // Rotate with the left stick
+            if (inputVector.magnitude > 0.001f)
+            {
+                // Gradually rotate until we're facing the direction the stick
+                // is pointing
+                float targetAngleDeg = Mathf.Atan2(inputVector.z, inputVector.x) * Mathf.Rad2Deg;
+                float hAngleDeg = _hAngle * Mathf.Rad2Deg;
+                hAngleDeg = Mathf.MoveTowardsAngle(hAngleDeg, targetAngleDeg, _rotSpeedDeg * Time.deltaTime);
+
+                // ...unless we're going really slow, then just pivot instantly.
+                if (_hSpeed < 0.1f)
+                    hAngleDeg = targetAngleDeg;
+
+                _hAngle = hAngleDeg * Mathf.Deg2Rad;
+            }
+        }
+        else
+        {
+            // TODO: different controls for when we're in the air
+        }
+    }
+
+    /// <summary>
+    /// Returns a vector representing the left control stick, relative to camera
+    /// space.
+    /// </summary>
+    /// <returns></returns>
+    private Vector3 GetWalkInput()
+    {
+        var rawInput = new Vector3
+        (
+            Input.GetAxisRaw("Horizontal"),
+            0,
+            Input.GetAxisRaw("Vertical")
+        );
+        
+        // Rotate it into camera space
+        var adjustedInput = 
+            Camera.main.transform.forward * rawInput.z +
+            Camera.main.transform.right * rawInput.x;
+        
+        // "flatten" it so that the y-coordinate is zero, while preserving the
+        // magnitude.
+        adjustedInput.y = 0;
+        adjustedInput.Normalize();
+        adjustedInput *= rawInput.magnitude;
+
+        return adjustedInput;
+    }
+
+    /// <summary>
+    /// Capsules suck for platformer collision.
+    /// Players demand that their feet be CYLINDERS!
+    /// </summary>
+    /// <returns></returns>
+    private bool IsGrounded()
+    {
+        return GetGround() != null;
+    }
+
+    /// <summary>
+    /// Returns the Transform of the ground that we're standing on,
+    /// or null if we're in the air.
+    /// </summary>
+    /// <returns></returns>
+    private Transform GetGround()
+    {
+        Vector3 origin = transform.position;
+        var hits = CylinderPhysics.CylinderCastAll(
+            transform.position,
+            _groundDetectorRadius,
+            _groundDetectorThickness,
+            Vector3.down,
+            _groundDetectorThickness / 2
+        );
+
+        foreach (var h in hits)
+        {
+            if (h.collider.transform != this.transform)
+                return h.collider.transform;
+        }
+        return null;
+    }
+}
