a406a699c780b308188ffde3fd70a9a497dc6b16
Alex Shelley <alexthepeppershelley@gmail.com>
2020-10-10 14:22:19 -0400

    Add SpinningPlatform script

---
+++ Assets/Scripts/SpinningPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningPlatform : MonoBehaviour
+{
+    // Degrees per second
+    public Vector3 RotSpeed = Vector3.zero;
+
+    public void FixedUpdate()
+    {
+        var angles = transform.localEulerAngles;
+        angles += RotSpeed * Time.deltaTime;
+        transform.localEulerAngles = angles;
+    }
+}
