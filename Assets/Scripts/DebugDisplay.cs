f5d03f72827a9ca733ad81c9e95e6001f55de4c8
Alex Shelley <alexthepeppershelley@gmail.com>
2020-10-18 16:21:49 -0400

    Add DebugDisplay.DrawCube

--- Assets/Scripts/DebugDisplay.cs
+++ Assets/Scripts/DebugDisplay.cs
@@ -21,6 +21,71 @@ public class DebugDisplay : MonoBehaviour
         _queuedFixedLines += line + "\n";
     }
 
+    public static void DrawCube(
+        Color color,
+        Vector3 center,
+        Vector3 halfExtents,
+        Quaternion? orientation = null
+    )
+    {
+        if (orientation == null)
+            orientation = Quaternion.identity;
+
+        // Generate all the corners
+        Vector3 startCorner = center - halfExtents;
+        Vector3[] corners = new[]
+        {
+            new Vector3(-1, 1, 1),
+            new Vector3(1, 1, 1),
+            new Vector3(-1, 1, -1),
+            new Vector3(1, 1, -1),
+            new Vector3(-1, -1, 1),
+            new Vector3(1, -1, 1),
+            new Vector3(-1, -1, -1),
+            new Vector3(1, -1, -1),
+        };
+        for (int i = 0; i < corners.Length; i++)
+        {
+            corners[i].x *= halfExtents.x;
+            corners[i].y *= halfExtents.y;
+            corners[i].z *= halfExtents.z;
+
+            corners[i] = orientation.Value * corners[i];
+            corners[i] += center;
+        }
+        // {
+        //     new Vector3(0, 0, 0),
+        //     new Vector3(0, 0, 1),
+        //     new Vector3(0, 1, 0),
+        //     new Vector3(0, 1, 1),
+        //     new Vector3(1, 0, 0),
+        //     new Vector3(1, 0, 1),
+        //     new Vector3(1, 1, 0),
+        //     new Vector3(1, 1, 1)
+        // };
+        // for(int i = 0; i < corners.Length; i++)
+        // {
+        //     corners[i] *= 2;
+        //     corners[i] -= Vector3.one;
+            
+        //     corners[i].x *= halfExtents.x;
+        //     corners[i].y *= halfExtents.y;
+        //     corners[i].z *= halfExtents.z;
+        //     corners[i] = orientation.Value * corners[i];
+
+        //     corners[i] += center;
+        // }
+
+        // Draw a line between each corner and every other corner
+        for (int i = 0; i < corners.Length; i++)
+        {
+            for (int j = i + 1; j < corners.Length; j++)
+            {
+                Debug.DrawLine(corners[i], corners[j], color);
+            }
+        }
+    }
+
     public void OnGUI()
     {
         GUILayout.BeginVertical("Box");
