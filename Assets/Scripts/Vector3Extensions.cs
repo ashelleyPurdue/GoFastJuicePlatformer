b80281b1110ed8fc8944d8e628fbed17dcbd94bd
Alex Shelley <alexthepeppershelley@gmail.com>
2020-11-14 13:48:26 -0500

    Add Vector3Extensions.Flattened()

---
+++ Assets/Scripts/Vector3Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector3Extensions
+{
+    public static Vector3 Flattened(this Vector3 v)
+    {
+        v.y = 0;
+        return v;
+    }
+}
