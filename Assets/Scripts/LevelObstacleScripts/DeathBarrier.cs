415a44c2446960ab86b0c073d56d8b8b8f5f7869
Alex Shelley <alexthepeppershelley@gmail.com>
2020-12-19 12:15:50 -0500

    Add death barrier object

---
+++ Assets/Scripts/LevelObstacleScripts/DeathBarrier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<PlayerController>();
+        if (player != null)
+            player.Kill();
+    }
+}
