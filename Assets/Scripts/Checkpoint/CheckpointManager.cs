b6fcffdf5c08e7ad814ed6d10cc211349ae57eea
Alex Shelley <alexthepeppershelley@gmail.com>
2020-12-19 12:50:33 -0500

    Add CheckpointManager and PlayerController.  Now you can die!

---
+++ Assets/Scripts/Checkpoint/CheckpointManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class CheckpointManager
+{
+    /// <summary>
+    /// Fired when the player reaches a checkpoint.
+    /// Lets subscribing objects know that they should save their state
+    /// so it can be reloaded after death
+    /// </summary>
+    public static event Action<CheckpointActivatedInfo> CheckpointActivated;
+
+    /// <summary>
+    /// Fired when the player has finished dieing and the screen has faded to
+    /// black.  Subscribers should revert back to their previously-saved state,
+    /// or their default state if no checkpoint has been reached.
+    /// </summary>
+    public static event Action Respawned;
+
+    static CheckpointManager()
+    {
+
+        SceneManager.sceneUnloaded += (Scene scene) =>
+        {
+            ClearSubscribers();
+        };
+    }
+
+    public static void ActivateCheckpoint(CheckpointActivatedInfo info)
+    {
+        CheckpointActivated?.Invoke(info);
+    }
+
+    public static void Respawn()
+    {
+        Respawned?.Invoke();
+    }
+
+    /// <summary>
+    /// Call this before changing scenes, to ensure unloaded objects get
+    /// garbage collected.
+    /// </summary>
+    private static void ClearSubscribers()
+    {
+        CheckpointActivated = null;
+        Respawned = null;
+    }
+}
+
+public struct CheckpointActivatedInfo
+{
+    public Vector3 PlayerRespawnPos;
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMovement _movement;
+
+    private Vector3 _respawnPosition;
+
+    void Awake()
+    {
+        // Subscribe to events
+        CheckpointManager.CheckpointActivated += OnCheckpointReached;
+        CheckpointManager.Respawned += OnRespawned;
+
+        // Get components
+        _movement = GetComponent<PlayerMovement>();
+
+        _respawnPosition = transform.position;
+    }
+
+    public void Kill()
+    {
+        // TODO: Play a dying animation, and then respawn AFTER it finishes.
+        CheckpointManager.Respawn();
+    }
+
+    private void OnCheckpointReached(CheckpointActivatedInfo info)
+    {
+        _respawnPosition = info.PlayerRespawnPos;
+    }
+
+    private void OnRespawned()
+    {
+        transform.position = _respawnPosition;
+        _movement.ResetState();
+        // TODO: Play a respawning animation
+    }
+}
