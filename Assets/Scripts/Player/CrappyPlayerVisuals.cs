763304f09550e1e27fc44edf56082000d6090000
Alex Shelley <alexthepeppershelley@gmail.com>
2020-12-20 14:08:53 -0500

    Model rotations now happen gradually

--- Assets/Scripts/Player/CrappyPlayerVisuals.cs
+++ Assets/Scripts/Player/CrappyPlayerVisuals.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerWallDetector))]
 public class CrappyPlayerVisuals : MonoBehaviour
 {
+    private const float MODEL_ROT_SPEED_DEG_PER_SEC = 720;
+
     public Transform _model;
     public Animator _animator;
 
@@ -14,11 +16,15 @@ public class CrappyPlayerVisuals : MonoBehaviour
     private PlayerGroundDetector _ground;
     private PlayerWallDetector _wall;
 
+    private Quaternion _targetRot;
+
     void Awake()
     {
         _movement = GetComponent<PlayerMovement>();
         _ground = GetComponent<PlayerGroundDetector>();
         _wall = GetComponent<PlayerWallDetector>();
+
+        _targetRot = _model.transform.localRotation;
     }
 
     void Update()
@@ -34,6 +40,11 @@ public class CrappyPlayerVisuals : MonoBehaviour
             FaceHAngle();
         else
             FaceWallSlide();
+
+        // Rotate toward the target rot
+        var rot = _model.transform.localRotation;
+        rot = Quaternion.RotateTowards(rot, _targetRot, MODEL_ROT_SPEED_DEG_PER_SEC * Time.deltaTime);
+        _model.transform.localRotation = rot;
     }
 
     private void UpdateAnimatorParams()
@@ -49,30 +60,31 @@ public class CrappyPlayerVisuals : MonoBehaviour
 
     private void FaceWallSlide()
     {
-        _model.forward = _wall.LastWallNormal.Flattened();
+        var forward = _wall.LastWallNormal.Flattened();
+        _targetRot = Quaternion.LookRotation(forward);
     }
 
     private void FaceHAngle()
     {
-        var eulers = _model.localEulerAngles;
+        var eulers = _targetRot.eulerAngles;
         eulers.y = -_movement.HAngleDeg + 90;
-        _model.localEulerAngles = eulers;
+        _targetRot.eulerAngles = eulers;
     }
 
     private void TiltWithSpeed()
     {
         float speedPercent = _movement.HSpeed / PlayerMovement.HSPEED_MAX_GROUND;
 
-        var eulers = _model.localEulerAngles;
+        var eulers = _targetRot.eulerAngles;
         eulers.x = SignedPow(speedPercent, 3) * 20;
-        _model.localEulerAngles = eulers;
+        _targetRot.eulerAngles = eulers;
     }
 
     private void DoNotTilt()
     {
-        var eulers = _model.localEulerAngles;
+        var eulers = _targetRot.eulerAngles;
         eulers.x = 0;
-        _model.localEulerAngles = eulers;
+        _targetRot.eulerAngles = eulers;
     }
 
     private float SignedPow(float f, float p)
