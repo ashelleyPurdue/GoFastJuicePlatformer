631eb60d046f97848f37b81d74e0eeeb2eec7506
Alex Shelley <alexthepeppershelley@gmail.com>
2020-12-30 11:07:20 -0500

    Remove the zooming logic

--- Assets/Scripts/Player/DumbOrbitCamera.cs
+++ Assets/Scripts/Player/DumbOrbitCamera.cs
@@ -16,7 +16,6 @@ public class DumbOrbitCamera : MonoBehaviour
     private const float MIN_VANGLE_DEG = -180;
     private const float MAX_VANGLE_DEG = 180;
     private const float ORBIT_RADIUS = 15;
-    private const float ZOOM_SPEED = 10;
 
     // Services
     private IPlayerInput _input;
@@ -24,7 +23,6 @@ public class DumbOrbitCamera : MonoBehaviour
     // State variables
     private float _hAngleDeg;
     private float _vAngleDeg;
-    private float _zoomDistance = ORBIT_RADIUS;
 
     void Awake()
     {
@@ -53,47 +51,9 @@ public class DumbOrbitCamera : MonoBehaviour
         Vector3 dir = SphericalToCartesian(_hAngleDeg, _vAngleDeg, 1);
         Vector3 pos = _target.position + (dir * ORBIT_RADIUS);
 
-        // Zoom in if the view is obstructed
-        float targetZoomDistance = GetTargetZoomDistance(pos, dir);
-
-        _zoomDistance = Mathf.MoveTowards(
-            _zoomDistance,
-            targetZoomDistance,
-            ZOOM_SPEED * Time.deltaTime
-        );
-
-        pos = _target.position + (_zoomDistance * dir);
-        
         // Jump to the position and look at the thing
         transform.position = pos;
         transform.LookAt(_target);
-    }
-
-    private float GetTargetZoomDistance(
-        Vector3 pos,
-        Vector3 dir
-    )
-    {
-        RaycastHit[] hits = Physics.RaycastAll(
-            pos,
-            -dir,
-            ORBIT_RADIUS
-        );
-
-        // If there were no obstructions, use the default distance
-        if (hits.Length == 0)
-            return ORBIT_RADIUS;
-
-        // Find the obstruction that's closest to the player
-        // (IE: furthest from the camera)
-        float furthestDist = float.MinValue;
-        foreach (var hit in hits)
-        {
-            if (hit.distance > furthestDist)
-                furthestDist = hit.distance;
-        }
-        
-        return ORBIT_RADIUS - furthestDist;
     }
 
     private Vector3 SphericalToCartesian(float hAngleDeg, float vAngleDeg, float orbitRaidus)
