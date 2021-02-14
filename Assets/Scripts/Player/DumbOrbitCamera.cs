a651cda65246b4dd2d86c27036290810081135cf
Alex Shelley <alexthepeppershelley@gmail.com>
2021-02-14 14:02:04 -0500

    Make the zooming logic smarter:
    * It is now satisfied with EITHER the head or feet being visible
    * It now zooms in faster than it zooms out.

--- Assets/Scripts/Player/DumbOrbitCamera.cs
+++ Assets/Scripts/Player/DumbOrbitCamera.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(IPlayerInput))]
@@ -16,7 +17,8 @@ public class DumbOrbitCamera : MonoBehaviour
     private const float MIN_VANGLE_DEG = -180;
     private const float MAX_VANGLE_DEG = 180;
     private const float ORBIT_RADIUS = 15;
-    private const float ZOOM_SPEED = 10;
+    private const float ZOOM_IN_SPEED = 50;
+    private const float ZOOM_OUT_SPEED = 10;
 
     // Services
     private IPlayerInput _input;
@@ -54,12 +56,20 @@ public class DumbOrbitCamera : MonoBehaviour
         Vector3 pos = _target.position + (dir * ORBIT_RADIUS);
 
         // Zoom in if the view is obstructed
-        float targetZoomDistance = GetTargetZoomDistance(pos, dir);
+        float targetZoomDistance = GetZoomDistance(
+            pos,
+            _target.position,
+            _target.position + (Vector3.up * PlayerConstants.BODY_HEIGHT)
+        );
+
+        float zoomSpeed = targetZoomDistance < _zoomDistance
+            ? ZOOM_IN_SPEED
+            : ZOOM_OUT_SPEED;
 
         _zoomDistance = Mathf.MoveTowards(
             _zoomDistance,
             targetZoomDistance,
-            ZOOM_SPEED * Time.deltaTime
+            zoomSpeed * Time.deltaTime
         );
 
         pos = _target.position + (_zoomDistance * dir);
@@ -69,31 +79,66 @@ public class DumbOrbitCamera : MonoBehaviour
         transform.LookAt(_target);
     }
 
-    private float GetTargetZoomDistance(
-        Vector3 pos,
-        Vector3 dir
+    private float GetZoomDistance(
+        Vector3 unzoomedCameraPos,
+        Vector3 targetFeetPos,
+        Vector3 targetHeadPos
     )
     {
-        RaycastHit[] hits = Physics.RaycastAll(
-            pos,
-            -dir,
-            ORBIT_RADIUS
-        );
+        const int MAX_ITERATIONS = 10;
 
-        // If there were no obstructions, use the default distance
-        if (hits.Length == 0)
-            return ORBIT_RADIUS;
-
-        // Find the obstruction that's closest to the player
-        // (IE: furthest from the camera)
-        float furthestDist = float.MinValue;
-        foreach (var hit in hits)
+        Vector3 cameraPos = unzoomedCameraPos;
+        for (int i = 0; i < MAX_ITERATIONS; i++)
         {
-            if (hit.distance > furthestDist)
-                furthestDist = hit.distance;
+            RaycastHit headHit;
+            RaycastHit feetHit;
+
+            bool isHeadObstructed = Physics.Raycast(
+                cameraPos,
+                (targetHeadPos - cameraPos).normalized,
+                out headHit,
+                Vector3.Distance(targetHeadPos, cameraPos)
+            );
+
+            bool areFeetObstructed = Physics.Raycast(
+                cameraPos,
+                (targetFeetPos - cameraPos).normalized,
+                out feetHit,
+                Vector3.Distance(targetFeetPos, cameraPos)
+            );
+
+            // If either the head or the feet are visible, then this is a good
+            // zoom distance.
+            if (!isHeadObstructed || !areFeetObstructed)
+                return Vector3.Distance(cameraPos, targetFeetPos);
+
+            // Both the head and the feet are blocked, so we need to zoom in
+            // more.  There are two points we can zoom to from here:
+            // * A point determined by the thing obstructing the head
+            // * A point determined by the thing obstructing the feet
+            // We will choose whichever point is closer to the camera pos.
+
+            // Find the point determined by the head obstructor.
+            var headDelta = Vector3.Project(
+                headHit.point - cameraPos,
+                targetFeetPos - cameraPos
+            );
+            Vector3 headZoomPos = cameraPos + headDelta;
+            float headZoomPosDist = Vector3.Distance(cameraPos, headZoomPos);
+
+            // Find the point determined by the feet obstructor
+            Vector3 feetZoomPos = feetHit.point;
+            float feetZoomPosDist = feetHit.distance;
+
+            // Move to whichever one is closest to the camera
+            if (feetZoomPosDist <= headZoomPosDist)
+                cameraPos = feetZoomPos;
+            else
+                cameraPos = headZoomPos;
         }
-        
-        return ORBIT_RADIUS - furthestDist;
+
+        // We couldn't find a good zoom point, so just give up.
+        return Vector3.Distance(cameraPos, targetFeetPos);
     }
 
     private Vector3 SphericalToCartesian(float hAngleDeg, float vAngleDeg, float orbitRaidus)
