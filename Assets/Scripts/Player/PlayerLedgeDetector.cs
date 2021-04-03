42091beb1547d2101823b945fbe72cef85479281
Alex Shelley <alexthepeppershelley@gmail.com>
2021-04-03 13:45:48 -0400

    Remove PlayerLedgeDetector's unnecessary dependency on PlayerStateMachine

--- Assets/Scripts/Player/PlayerLedgeDetector.cs
+++ Assets/Scripts/Player/PlayerLedgeDetector.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerStateMachine))]
 public class PlayerLedgeDetector : MonoBehaviour
 {
     private const float BIG_NUMBER = 10000;
