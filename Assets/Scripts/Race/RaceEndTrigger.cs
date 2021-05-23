5eba399a965a1d0bae32c3d8c99db406c127b7ac
Alex Shelley <alexthepeppershelley@gmail.com>
2021-05-23 11:47:12 -0400

    Add RaceEndTrigger.  Move RaceStartTrigger to a Race folder.

--- Assets/Scripts/Race/RaceEndTrigger.cs
+++ Assets/Scripts/Race/RaceEndTrigger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RaceStartTrigger : MonoBehaviour
+public class RaceEndTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
